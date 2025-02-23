03586266aa5aa69e80a01b7bd0b5fac11901e98d
Mohammed Saleh <mohammedsaleh002@outlook.com>
2025-02-23 13:39:41 +0200

    refactor(backend): move app.controller.js to src and implement short polling endpoint

--- server/src/app.controller.js
+++ server/src/app.controller.js
@@ -3,6 +3,10 @@ const bootstrap = (app, express) => {
 
   app.use(express.json());
 
+  app.get("/short-polling", (req, res, next) => {
+    return res.status(200).json({ message: "short-polling" });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
